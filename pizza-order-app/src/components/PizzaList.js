cb251cc3d7eda822805ec47e88f284519db0a17a
SegFault255 <grayba423@gmail.com>
2019-06-08 17:59:28 -0400

    Add util to get topping prices and update order list to show topping prices. Still need to add base pizza price

--- pizza-order-app/src/components/PizzaList.js
+++ pizza-order-app/src/components/PizzaList.js
@@ -1,20 +1,30 @@
 import * as React from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
+import toppingPrices from "../utils/toppingPrices";
 
 const PizzaList = ({ pizzas }) => {
     const [mappedPizzas, setMappedPizzas] = React.useState(null);
+    const [toppings, setToppings] = React.useState([]);
+
+    React.useEffect(() => {
+        toppingPrices().then(result => setToppings(result));
+    }, []);
 
     React.useEffect(() => {
         setMappedPizzas(
             pizzas.map(p => {
                 return {
                     ...p,
+                    price: toppings
+                        .filter(t => p.toppings.find(pt => pt.name === t.name))
+                        .map(t => t.price)
+                        .reduce((p, c) => p + c, p.price),
                     toppings: p.toppings.map(t => t.name).join(", ")
                 };
             })
         );
-    }, [pizzas]);
+    }, [pizzas, toppings]);
 
     return (
         <DataTable value={mappedPizzas}>
